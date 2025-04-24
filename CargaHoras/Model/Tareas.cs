eaea61155cd1fcca4a31461a718420a971865a05
Mariano Aluzzo <mariano.aluzzo@alusoft.com.ar>
2025-04-23 21:20:23 -0300

    Add Validacion

--- CargaHoras/Model/Tareas.cs
+++ CargaHoras/Model/Tareas.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Runtime.InteropServices;
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace CargaHoras.Model
 {
@@ -9,15 +10,20 @@ namespace CargaHoras.Model
         public string Usuario { get; set; }= "";
         public int id { get; set; }
         public DateTime fecha { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage ="Debe seleccionar un cliente")]
         public int? clienteId { get; set; }
         public int? areaFuncionalId { get; set; }
 
-        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [Required(ErrorMessage = "Debe ingresar una tarea")]
         public string tarea { get; set; } = "";
 
         public int? patenteId { get; set; }
+
+        [Required(ErrorMessage = "La seleccionar un producto")]
         public int? productoId { get; set; }
         [Required(ErrorMessage = "La Hora es obligatorio")]
+        [Range(1, 24, ErrorMessage = "Las horas deben estar entre 1 y 24")]
         public int horas { get; set; }
         public int minutos { get; set; } = 0;
 
